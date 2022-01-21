966962e46078edfe01b90ab7ce4fdd1cde65b7c0
ben <moon791017@gmail.com>
2022-01-21 14:10:16 +0800

    add theme

--- web_site/pages/_app.tsx
+++ web_site/pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes';
+import ThemeSwitch from './components/theme-switch';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-slate-50 dark:bg-black">
+      <ThemeProvider attribute="class">
+        <header className="py-2">
+          <div className="flex justify-between items-center">
+            <h2 className='text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight'>dashboard</h2>
+            <ThemeSwitch />
+          </div>
+        </header>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </div>
+  )
 }
 
 export default MyApp
--- web_site/tailwind.config.js
+++ web_site/tailwind.config.js
@@ -1,10 +1,30 @@
 module.exports = {
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        dark: {
+          css: {
+            color: theme('colors.gray.300'),
+            h1: {
+              color: theme('colors.gray.100'),
+            },
+            h2: {
+              color: theme('colors.gray.100'),
+            },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ['dark'],
+    },
   },
   plugins: [],
 }
