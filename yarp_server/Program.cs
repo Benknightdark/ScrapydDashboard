0be32e3c1ed3df83e86be8ba70c0da9cd8987b8f
ben <moon791017@gmail.com>
2022-05-16 17:09:03 +0800

    dd

--- yarp_server/Program.cs
+++ yarp_server/Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.AspNetCore.Authentication;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOcelot();
+builder.Services.AddAuthentication("BasicAuthentication")
+         .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+builder.Services.AddScoped<IUserService, UserService>();
+
+
 var app = builder.Build();
 app.UseOcelot().Wait();
+// app.UseCors(x => x
+//     .AllowAnyOrigin()
+//     .AllowAnyMethod()
+//     .AllowAnyHeader()
+//     .AllowCredentials());
+app.UseAuthentication();
 app.Run();
