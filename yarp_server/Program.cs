e57fe8162986a2dc18615d73b8324f4fcb5faa72
ben <moon791017@gmail.com>
2022-05-16 14:19:01 +0800

    test

--- yarp_server/Program.cs
+++ yarp_server/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 var app = builder.Build();
+app.UseHttpLogging();
 app.MapReverseProxy();
 app.Run();
